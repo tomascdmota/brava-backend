c1c534b42c3110b77077de22853360279dc9ac79
tomascdmota <tomascdmota@gmail.com>
2023-12-05 19:22:53 +0000

    Minor updates to cors

--- index.js
+++ index.js
@@ -5,40 +5,57 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 const app = express();
-
+app.use(cookieParser());
 dotenv.config();
 const allowedOrigins = [
-  'http://192.168.1.155:3000',
-  'https://192.168.1.155:3000',
-  'http://10.11.66.111:3000',
+  'http://18.133.155.66:3000',
+  'https://18.133.155.66:3000',
   'https://brava-bucket.s3.eu-west-2.amazonaws.com',
 ];
 
-
-
-
-
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-      return callback(new Error(msg), false);
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
-
-    return callback(null, true);
   },
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-const PORT = 3306;
+app.use((req,res,next) => {
+  console.log('Request Headers:', req.headers);
+  next();
+});
+
+const PORT = 4001;
+
+
+app.use((req, res, next) => {
+  console.log('Request Headers:', req.headers);
+  console.log('Cookies:', req.cookies);
+  next();
+});
+
 
 app.use(express.json());
-app.use(cookieParser());
-app.use("/api", router);
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+
+  next();
+});
+app.use("/api", router, (req,res,next)=> {console.log('REQUEST HEADERS:', req.headers); next()}) ;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
